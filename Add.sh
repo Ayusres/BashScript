5610b598520095c312b8172b5579ec092895bb2d
Ayusres <ayushrestha.2002@gmail.com>
2024-12-04 15:57:39 +0545

    Modified

--- Add.sh
+++ Add.sh
@@ -2,6 +2,9 @@
 echo "Enter 1st Number"
 read Num1
 echo "Enter 2nd Number"
-read Num2 
-sum=$((Num1 + Num2))
+read Num2
+echo "Enter 3rd Number"
+read Num3
+
+sum=$((Num1 + Num2 + Num3))
 echo "The Sum of Two Number is "$sum 
